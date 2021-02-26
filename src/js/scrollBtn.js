240f843493b744863754cd5bc3611a9a1739b372
Nichkov Roman <nichkovroman@gmail.com>
2021-02-26 18:43:30 +0300

    scroll button js

---
+++ src/js/scrollBtn.js
@@ -0,0 +1,9 @@
+const scrollBtn = () =>{
+    const header = document.querySelector('.header');
+    const introDownBtn = document.querySelector('.intro__down');
+    const aboutBlock = document.querySelector('.section--about');
+    introDownBtn.addEventListener('click', ()=>{
+        aboutBlock.scrollIntoView({ behavior: "smooth"});
+    })
+}
+scrollBtn();
