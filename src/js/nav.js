3eca03bf4c047532bab6cb6d155fe09fb6685022
Nichkov Roman <nichkovroman@gmail.com>
2021-02-26 19:42:45 +0300

    header background fix. adding bg before scrolling

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,6 +1,12 @@
 const nav = ()=>{
     const header = document.querySelector('.header');
     const intro = document.querySelector('.intro');
+
+    if(pageYOffset > intro.clientHeight / 5){
+        header.classList.add('active');
+    }
+    else header.classList.remove('active');
+
     window.addEventListener('scroll', () =>{
         if(pageYOffset > intro.clientHeight / 5){
             header.classList.add('active');
