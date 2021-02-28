0315f6fde8be495879fb83b9cd190451624f5128
Nichkov Roman <nichkovroman@gmail.com>
2021-02-28 22:39:58 +0300

    toggle nav v1

--- src/js/toggleMenu.js
+++ src/js/toggleMenu.js
@@ -1,7 +1,10 @@
 const toggleMenu = ()=>{
     const burgerBtn = document.querySelector('.burger');
+    const nav = document.querySelector('.nav');
     burgerBtn.addEventListener('click', ()=>{
         burgerBtn.classList.toggle('active');
+        nav.classList.toggle('active');
+        document.body.classList.toggle('no-scroll');
     })
 }
 toggleMenu();
