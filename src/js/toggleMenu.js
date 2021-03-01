c4bbe5d58dd329da73a8c7ea90b711216c42d753
Nichkov Roman <nichkovroman@gmail.com>
2021-03-01 09:49:53 +0300

    toggle menu close after click the link

--- src/js/toggleMenu.js
+++ src/js/toggleMenu.js
@@ -1,10 +1,19 @@
 const toggleMenu = ()=>{
     const burgerBtn = document.querySelector('.burger');
     const nav = document.querySelector('.nav');
+    const navLinks = document.querySelectorAll('.nav__link');
     burgerBtn.addEventListener('click', ()=>{
         burgerBtn.classList.toggle('active');
         nav.classList.toggle('active');
         document.body.classList.toggle('no-scroll');
+
+        navLinks.forEach(item =>{
+            item.addEventListener('click', ()=>{
+                burgerBtn.classList.remove('active');
+                nav.classList.remove('active');
+                document.body.classList.remove('active');
+            })
+        })
     })
 }
 toggleMenu();
