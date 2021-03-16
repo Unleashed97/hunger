c95b1b5e26421cb4beaeed31bc7315fe2ceb7a74
Nichkov Roman <nichkovroman@gmail.com>
2021-03-16 14:32:06 +0300

    image optimizing. png to jpg + gulpfile changed

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const { src, dest, parallel, series, watch } = require('gulp');
 const sync = require('browser-sync').create();
 const concat = require('gulp-concat');
@@ -7,6 +9,8 @@ const autoprefixer = require('gulp-autoprefixer');
 const cleancss = require('gulp-clean-css');
 const del = require('del');
 const htmlmin = require('gulp-htmlmin');
+const imagemin = require('gulp-imagemin');
+const newer = require('gulp-newer');
 const rename = require('gulp-rename');
 
 // HTML
@@ -23,35 +27,61 @@ const html = () => {
 // Styles
 const styles = () => {
     return src(['src/scss/main.scss'])
-    .pipe(sass())
-    .pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], cascade: false }))
-    .pipe(cleancss(({ level: { 1: { specialComments: 0}}})))
-    .pipe(rename({
-        basename: 'style',
-        suffix: '.min'
-      }))
-    .pipe(dest('src/css/'))
-    .pipe(dest('dist/css/'))
-    .pipe(sync.stream())
+        .pipe(sass())
+        .pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], cascade: false }))
+        .pipe(cleancss(({ level: { 1: { specialComments: 0}}})))
+        .pipe(rename({
+            basename: 'style',
+            suffix: '.min'
+        }))
+        .pipe(dest('src/css/'))
+        .pipe(dest('dist/css/'))
+        .pipe(sync.stream())
 }
 
 // Scripts
 const scripts = () => {
     return src(['src/js/script.js', 'src/js/**/*.js', '!src/js/script.min.js'])
-    .pipe(concat('script.min.js'))
-    .pipe(uglify())
-    .pipe(dest('src/js/'))
-    .pipe(dest('dist/js/'))
-    .pipe(sync.stream())
+        .pipe(concat('script.min.js'))
+        .pipe(uglify())
+        .pipe(dest('src/js/'))
+        .pipe(dest('dist/js/'))
+        .pipe(sync.stream())
 }
 
 // server
 const server = () => {
     sync.init({
-        server: { baseDir: 'src'},
+        server: { baseDir: 'dist/'},
         notify: false,
         online: true
     })
+}
+
+// image minify
+const images = () =>{
+    return src('src/images/**/*.{jpg,png,svg,gif,ico,webp,webmanifest,xml,json}')
+        .pipe(newer('dist/images/'))
+        .pipe(imagemin([
+            imagemin.gifsicle({interlaced: true}),
+            imagemin.mozjpeg({quality: 75, progressive: true}),
+            imagemin.optipng({optimizationLevel: 5}),
+            imagemin.svgo({
+                plugins: [
+                    {removeViewBox: true},
+                    {cleanupIDs: false}
+                ]
+            })
+        ]))
+        .pipe(dest('dist/images/'))
+        .pipe(sync.stream())
+}
+
+// fonts
+const fonts = () =>{
+    return src('src/fonts/**/*.{eot,woff,woff2,ttf,svg}')
+        .pipe(dest('/dist/fonts/'))
+        .pipe(sync.stream())
 }
 
 // Copy
@@ -62,8 +92,8 @@ const copy = () => {
     ], {
         base: 'src'
     })
-    .pipe(dest('dist/'))
-    .pipe(sync.stream());
+        .pipe(dest('dist/'))
+        .pipe(sync.stream());
 }
 
 // cleandist
@@ -71,33 +101,27 @@ const cleandist = () => {
     return del('dist/**/*', { force: true })
 }
 
-// build copy
-// function buildcopy() {
-//     return src(['src/css/**/*.min.css',
-//         'src/js/**/*.min.js',
-//         'src/images/**/*',
-//         'src/fonts/**/*',
-//         'src/**/*.html',
-//         ], { base: 'src' })
-//     .pipe(dest('dist'))
-// }
-
 // Watch
-const startwatch = () => {
+const watchFiles = () => {
     watch('src/*.html', html);
     watch('src/**/*.scss', styles);
     watch(['src/**/*.js', '!src/js/script.min.js'], scripts);
-    watch(['src/images/**/*', 'src/fonts/**/*'], copy);
+    watch('src/images/**/*.{jpg,png,svg,gif,ico,webp,webmanifest,xml,json}', images)
+    watch('src/fonts/**/*.{eot,woff,woff2,ttf,svg}', fonts);
     watch('src/**/*.html').on('change', sync.reload);
 }
 
+const build = series(cleandist, parallel(html, styles, scripts, images));
+const watcher = parallel(build, watchFiles, server);
+
+
+// Export tasks
 exports.server = server;
 exports.html = html;
 exports.scripts = scripts;
 exports.styles = styles;
 exports.copy = copy;
+exports.images = images;
 exports.cleandist = cleandist;
-
-exports.build = series(cleandist, parallel(html, styles, scripts));
-
-exports.default = parallel(startwatch, server);
+exports.build = build;
+exports.default = watcher;
